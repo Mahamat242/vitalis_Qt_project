a85bf2d2853caa1ae434c840cd53f99b882ef29b
Mahamat242 <harounm087@gmail.com>
2026-08-19 15:46:12 +0000

    ok

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,32 +13,22 @@ MainWindow::MainWindow(QWidget *parent)
     navMenuLayout->addWidget(btnConsultation);
     navMenuLayout->addStretch();
 
-    // 3. Conteneur multi-pages (QStackedWidget) à droite
     stackedWidget = new QStackedWidget(this);
-
-    // Instanciation du formulaire de consultation
     consultationPage = new ConsultationForm(this);
 
-    // Ajout de la page au QStackedWidget
     stackedWidget->addWidget(consultationPage);
 
-    // 4. Assemblage dans le layout principal
-    mainLayout->addLayout(navMenuLayout, 1);     // Menu prend 1/5 de la largeur
-    mainLayout->addWidget(stackedWidget, 4);    // Contenu prend 4/5 de la largeur
+    mainLayout->addLayout(navMenuLayout, 1);
+    mainLayout->addWidget(stackedWidget, 4);
 
-    // 5. Connexion du bouton au changement de page
     connect(btnConsultation, &QPushButton::clicked, this, &MainWindow::showConsultationPage);
 
-    // Titre et dimensions de la fenêtre
-    setWindowTitle("Vitalis - Gestion Médicale");
+    setWindowTitle("Vitalis - Gestion médicale");
     resize(900, 600);
 }
 
 void MainWindow::showConsultationPage()
 {
-    // Affiche la page dans le stackedWidget
     stackedWidget->setCurrentWidget(consultationPage);
-
-    // Rafraîchit les listes déroulantes depuis la BDD (Médecins, Patients, Médicaments)
     consultationPage->refreshData();
 }
